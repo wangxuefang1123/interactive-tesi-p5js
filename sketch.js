06eb80b7f98c6df9731d4bf8de1cfd8079ab58cc
wangxuefang1123 <1306460615a@gmail.com>
2026-02-22 17:36:35 +0100

    xb

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 /* global p5 */
+
 let bgImg;
 let waterSound;
 
 let fishImgs = [];
 let fishes = [];
 
+//世界真实尺寸
+let worldW = 1920;
+let worldH = 1080;
+
 function preload() {
-  bgImg = loadImage("assets/lake_img.jpg");
+  bgImg = loadImage("assets/lake_img.png");
 
   soundFormats("wav");
   waterSound = loadSound("assets/water.wav");
@@ -17,66 +22,93 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(895, 540);
+  createCanvas(windowWidth, windowHeight);
 
-  // 创建三条鱼
+  // 创建鱼（在世界坐标中）
   for (let i = 0; i < 5; i++) {
     let img = random(fishImgs);
-    fishes.push(new Fish(random(width), random(height), img));
+    fishes.push(new Fish(random(worldW), random(worldH), img));
   }
 }
 
 function draw() {
-  image(bgImg, 0, 0, width, height);
+  background(0);
+
+  //等比缩放世界到屏幕
+  let scaleFactor = min(width / worldW, height / worldH);
+
+  push();
+
+  // 居中显示
+  translate(
+    (width - worldW * scaleFactor) / 2,
+    (height - worldH * scaleFactor) / 2,
+  );
+
+  scale(scaleFactor);
+
+  //背景按世界尺寸画
+  image(bgImg, 0, 0, worldW, worldH);
 
   for (let fish of fishes) {
     fish.update();
     fish.display();
   }
+
+  pop();
 }
 
-// 鼠标点击（电脑）
+//屏幕尺寸变化
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// 鼠标点击
 
 function mousePressed() {
   startSound();
-
-  for (let fish of fishes) {
-    fish.clicked(mouseX, mouseY);
-  }
+  handleTouch(mouseX, mouseY);
 }
 
-// 手指触摸（手机 / 平板）
+// 触屏
 
 function touchStarted() {
   startSound();
-
-  for (let fish of fishes) {
-    fish.clicked(mouseX, mouseY);
-  }
-
-  return false; // 防止页面滚动
+  handleTouch(mouseX, mouseY);
+  return false;
 }
 
-// 播放水声（浏览器需要用户触发）
+//把屏幕坐标转换为世界坐标
+function handleTouch(px, py) {
+  let scaleFactor = min(width / worldW, height / worldH);
+
+  let offsetX = (width - worldW * scaleFactor) / 2;
+  let offsetY = (height - worldH * scaleFactor) / 2;
+
+  let worldX = (px - offsetX) / scaleFactor;
+  let worldY = (py - offsetY) / scaleFactor;
+
+  for (let fish of fishes) {
+    fish.clicked(worldX, worldY);
+  }
+}
 
 function startSound() {
   userStartAudio();
-
   if (!waterSound.isPlaying()) {
     waterSound.loop();
     waterSound.setVolume(0.5);
   }
 }
 
-// 鱼类
-
+// 鱼
 class Fish {
   constructor(x, y, img) {
     this.pos = createVector(x, y);
     this.img = img;
 
-    this.normalSpeed = 0.8;
-    this.fastSpeed = 2.2;
+    this.normalSpeed = 2.0;
+    this.fastSpeed = 5.0;
     this.speed = this.normalSpeed;
 
     this.angle = random(TWO_PI);
@@ -84,17 +116,15 @@ class Fish {
 
     this.isFast = false;
     this.timer = 0;
-    this.fastDuration = 120; // 约2秒（60fps）
+    this.fastDuration = 120;
 
-    this.scaleFactor = 0.15;
+    this.scaleFactor = 0.3;
   }
 
   update() {
-    // 噪声控制方向变化
     this.angle += map(noise(this.noiseOffset), 0, 1, -0.02, 0.02);
     this.noiseOffset += 0.01;
 
-    // 速度微变化
     let speedVariation = map(noise(this.noiseOffset + 100), 0, 1, -0.1, 0.1);
     let currentSpeed = this.speed + speedVariation;
 
@@ -102,13 +132,12 @@ class Fish {
     v.mult(currentSpeed);
     this.pos.add(v);
 
-    // 边界循环
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+    // 使用世界边界
+    if (this.pos.x > worldW) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = worldW;
+    if (this.pos.y > worldH) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = worldH;
 
-    // 加速计时
     if (this.isFast) {
       this.timer++;
       if (this.timer > this.fastDuration) {
@@ -121,7 +150,6 @@ class Fish {
     push();
     translate(this.pos.x, this.pos.y);
 
-    // 尾巴摆动
     let tailSwing = sin(frameCount * 0.05 + this.noiseOffset * 5) * 0.05;
     rotate(this.angle + tailSwing);
 
@@ -133,11 +161,9 @@ class Fish {
     pop();
   }
 
-  // 点击 / 触摸检测
   clicked(mx, my) {
     let d = dist(mx, my, this.pos.x, this.pos.y);
 
-    // 点击范围（适合触屏）
     if (d < this.img.width * this.scaleFactor * 0.6) {
       this.speed = this.fastSpeed;
       this.isFast = true;
